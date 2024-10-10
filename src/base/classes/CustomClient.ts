ce7f08b2b5da04c8a270ee267a4f59727ac3ace9
Enzo G. Borrelli A. <enzoborrellidev@gmail.com>
2024-10-10 16:35:04 -0300

    added i18next init

--- src/base/classes/CustomClient.ts
+++ src/base/classes/CustomClient.ts
@@ -5,6 +5,8 @@ import Handler from "./Handler";
 import Command from "./Command";
 import SubCommand from "./SubCommand";
 import { connect } from "mongoose";
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
 
 export default class CustomClient extends Client implements ICustomClient {
   handler: Handler;
@@ -24,10 +26,11 @@ export default class CustomClient extends Client implements ICustomClient {
     this.developmentMode = process.argv.slice(2).includes("--development");
   }
 
-  init(): void {
+  async init(): Promise<void> {
     console.log(
       `${this.developmentMode ? "development" : "production"} mode enabled`
     );
+    await this.initializeI18n();
     this.LoadHandlers();
     this.login(
       this.developmentMode ? this.config.devToken : this.config.token
@@ -41,5 +44,17 @@ export default class CustomClient extends Client implements ICustomClient {
   LoadHandlers(): void {
     this.handler.LoadEvents();
     this.handler.LoadCommands();
+  }
+  async initializeI18n(): Promise<void> {
+    await i18next.use(Backend).init({
+      initImmediate: false,
+      lng: "en",
+      fallbackLng: "en",
+      backend: {
+        loadPath: "./src/locales/{{lng}}.json",
+      },
+      debug: true,
+    });
+    console.log("i18Next initialize with lang:", i18next.language);
   }
 }
